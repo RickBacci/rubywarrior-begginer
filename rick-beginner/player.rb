13bed3875c5009237d89a7f882a23299a1c1a678
Richard Bacci <brownieelf175@gmail.com>
2014-12-09 12:19:36 -0500

    level 7 cleared

--- rick-beginner/player.rb
+++ rick-beginner/player.rb
@@ -1,42 +1,46 @@
 class Player
   def play_turn(warrior)
-  	@found_a_wall = false if @found_a_wall.nil?
-
-  	if @found_a_wall
-  		direction = :forward
-  	else
-  		direction = :backward
-  	end
-
-    if @health.nil? || warrior.health >= @health
-    	i_am_being_attacked = false
-    else
-			i_am_being_attacked = true
+    @warrior = warrior
+    @health = 20 if @health.nil?
+    
+    def under_attack?
+      @warrior.health < @health
+    end
+    
+    def wounded?
+      @warrior.health != 20 
     end
 
-  	if warrior.feel(direction).empty?
-  		# there is nothing in front of me
-  		if warrior.health < 20 && i_am_being_attacked == false
-  			warrior.rest!
-  		else
-  			if warrior.health < 10
-  				if direction == :forward
-  					warrior.walk!(:backward)
-  				else
-  				  warrior.walk!(:forward)
-  				end
-  			else
-	     	  warrior.walk!(direction)
-	     	end
-  		end
-    else
-    	#check to see if this is a captive or sludge
-    	if warrior.feel(direction).captive? == true
-    		warrior.rescue!(direction)
-    	elsif warrior.feel(direction).wall?
-    		@found_a_wall = true
+    def severely_wounded?
+      @warrior.health <= 10
+    end
+
+    def retreat!
+      @warrior.walk!(:backward)
+    end
+
+    
+  	if warrior.feel.empty? # feel nothing near me
+      if under_attack?
+        if severely_wounded?
+          retreat!
+        else
+          warrior.walk!
+        end
+      else # not under attack
+        if wounded?
+          warrior.rest!
+        else
+          warrior.walk!
+        end
+      end
+    else # feel something near me
+    	if warrior.feel.captive?
+    		warrior.rescue!
+    	elsif warrior.feel.wall?
+        warrior.pivot!
     	else
-        warrior.attack!(direction)
+        warrior.attack!
       end
     end
 
