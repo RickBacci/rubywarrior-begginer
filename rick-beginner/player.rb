d065f28cd6adc4e4719e31e56bcb7c801e3671d9
Richard Bacci <brownieelf175@gmail.com>
2014-12-10 13:36:11 -0500

    complete lv 8

--- rick-beginner/player.rb
+++ rick-beginner/player.rb
@@ -1,5 +1,6 @@
 class Player
   def play_turn(warrior)
+    
     @warrior = warrior
     @health = 20 if @health.nil?
     
@@ -19,29 +20,71 @@ class Player
       @warrior.walk!(:backward)
     end
 
+
+    scout_area = []
+
+    scout_area << warrior.look[0] << warrior.look[1] << warrior.look[2]
     
+    @intel = []
+    scout_area.each do |space|
+      @intel << space.to_s
+    end
+    p @intel
+    p "captive ahead" if @intel[0] == 'Captive'
+    p "wizards present" if @intel.include?('Wizard')
+
+    def wizard?
+      @intel[1] == 'Wizard'
+      #@intel.include?('Wizard')
+    end
+
+    def captive?
+      @intel[1] == 'Captive'
+      #@intel.include?('Captive')
+    end
+
+    def trapped?
+      @warrior.feel(:backward).wall?
+    end
+
+
   	if warrior.feel.empty? # feel nothing near me
+
       if under_attack?
         if severely_wounded?
-          retreat!
+          if trapped?
+            p 'shoot from severely_wounded and trapped?'
+            @warrior.shoot!
+          else
+            p 'retreat because severely_wounded'
+            retreat!
+          end
         else
+          p 'walk while under attack and not severely_wounded'
           warrior.walk!
         end
       else # not under attack
         if wounded?
           warrior.rest!
         else
-          warrior.walk!
+          if captive?
+            p 'not under attack - see captive 2 spaces away'
+            warrior.walk!
+          else
+            if @intel.include?('Wizard')
+              warrior.shoot!
+            else
+              warrior.walk!
+            end 
+          end
         end
       end
-    else # feel something near me
-    	if warrior.feel.captive?
-    		warrior.rescue!
-    	elsif warrior.feel.wall?
-        warrior.pivot!
-    	else
-        warrior.attack!
-      end
+    elsif warrior.feel.captive?
+    	warrior.rescue!
+    elsif warrior.feel.wall?
+      warrior.pivot!
+    else
+      p 'What do you feel?'
     end
 
   	@health = warrior.health
