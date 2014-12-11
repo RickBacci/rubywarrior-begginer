14f643ddbdbb674152e37998e41c46f51b9a06ed
Richard Bacci <brownieelf175@gmail.com>
2014-12-10 19:34:43 -0500

    survives thru all levels with B average

--- rick-beginner/player.rb
+++ rick-beginner/player.rb
@@ -4,9 +4,11 @@ class Player
     @warrior = warrior
     @health = 20 if @health.nil?
     
-    def under_attack?
-      @warrior.health < @health
-    end
+    define_singleton_method('under_attack?') { warrior.health < @health }
+
+    # def under_attack?
+    #   @warrior.health < @health
+    # end
     
     def wounded?
       @warrior.health != 20 
@@ -21,33 +23,64 @@ class Player
     end
 
 
-    scout_area = []
-
-    scout_area << warrior.look[0] << warrior.look[1] << warrior.look[2]
+    scout_ahead = []
+    scout_ahead << warrior.look[0] << warrior.look[1] << warrior.look[2]
     
-    @intel = []
-    scout_area.each do |space|
-      @intel << space.to_s
-    end
-    p @intel
-    p "captive ahead" if @intel[0] == 'Captive'
-    p "wizards present" if @intel.include?('Wizard')
-    p "Archer present" if @intel.include?('Archer')
-
-    def wizard?
-      @intel[1] == 'Wizard' || @intel[2] == 'Wizard'
+    @intel_ahead = []
+    scout_ahead.each do |space|
+      @intel_ahead << space.to_s
     end
 
-    def captive?
-      @intel[1] == 'Captive'
+    scout_behind = []
+    scout_behind << warrior.look(:backward)[0] << warrior.look(:backward)[1] << warrior.look(:backward)[2]
+
+    @intel_behind = []
+    scout_behind.each do |space|
+      @intel_behind << space.to_s
     end
 
-    def archer?
-      @intel[1] == 'Archer' || @intel[2] == 'Archer'
+    # p "Intel_ahead: #{@intel_ahead}"
+    # p "Intel_behind: #{@intel_behind}"
+
+    # p "wizards ahead" if @intel_ahead.include?('Wizard')
+    # p "Archer ahead" if @intel_ahead.include?('Archer')
+    # p "Captive ahead" if @intel_ahead.include?('Captive')
+
+    # p "wizards behind" if @intel_behind.include?('Wizard')
+    # p "Archer behind" if @intel_behind.include?('Archer')
+    # p "Captive behind" if @intel_behind.include?('Captive')
+
+
+    def wizard_ahead?
+      @intel_ahead[1] == 'Wizard' || @intel_ahead[2] == 'Wizard'
+    end
+
+    def wizard_behind?
+      @intel_behind[1] == 'Wizard' || @intel_behind[2] == 'Wizard'
+    end
+
+    def captive_ahead?
+      @intel_ahead[1] == 'Captive'
+    end
+
+    def captive_behind?
+      @intel_behind[1] == 'Captive'
+    end
+
+    def archer_ahead?
+      @intel_ahead[1] == 'Archer' || @intel_ahead[2] == 'Archer'
+    end
+
+    def archer_behind?
+      @intel_behind[1] == 'Archer' || @intel_behind[2] == 'Archer'
     end
 
     def wall?
-      @intel[1] == 'wall'
+      @intel_ahead[1] == 'wall'
+    end
+
+    def wall_behind?
+      @intel_behind[1] == 'wall'
     end
 
     def trapped?
@@ -55,7 +88,7 @@ class Player
     end
 
 
-  	if warrior.feel.empty? # feel nothing near me
+  	if warrior.feel.empty?
 
       if under_attack?
         if severely_wounded?
@@ -68,28 +101,38 @@ class Player
           end
         else
           p 'walk while under attack and not severely_wounded'
-          if wizard? 
+          if wizard_ahead? 
             warrior.shoot!
+          elsif warrior.feel.enemy?
+            warrior.attack!
           else
             warrior.walk!
           end
         end
       else # warrior not under attack
         if wounded?
-          if @intel.include?('Wizard') || archer?
+          if @intel_ahead.include?('Wizard') || archer_ahead?
             warrior.shoot!
           else
             warrior.rest!
           end
         else
-          if captive?
+          if captive_ahead?
             p 'not under attack - see captive 2 spaces away'
-            warrior.walk!
-          # elsif wall?
-          #   warrior.pivot!
+            if warrior.feel.enemy?
+              warrior.attack!
+            elsif warrior.feel.captive?
+              warrior.rescue!
+            else
+              warrior.walk!
+            end
+          elsif captive_behind?
+            warrior.pivot!
           else
-            if wizard? || archer?
+            if wizard_ahead? || archer_ahead?
               warrior.shoot!
+            elsif warrior.feel.enemy?
+              warrior.attack!
             else
               warrior.walk!
             end 
@@ -98,9 +141,12 @@ class Player
       end
     elsif warrior.feel.captive?
     	warrior.rescue!
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue!(:backward)
     elsif warrior.feel.wall?
       warrior.pivot!
     elsif warrior.feel.enemy?
+      p 'close quarters fighting'
       warrior.attack!
     else
       warrior.walk!
