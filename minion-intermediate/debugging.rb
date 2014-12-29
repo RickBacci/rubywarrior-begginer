3dc6d83725e3ef46fc71eab9d0bc0b2a47e6d759
Richard Bacci <brownieelf175@gmail.com>
2014-12-28 21:53:16 -0500

    move debugging to separate file

---
+++ minion-intermediate/debugging.rb
@@ -0,0 +1,29 @@
+
+# p "direction of all enemies: #{@directions_to_all_enemies}"
+# p "direction to captive locations: #{@direction_to_captive_locations}"
+# p "total enemies in attack range: #{@total_enemies_in_attack_range}"
+# p "direction of enemies in attack range: #{@direction_of_enemies_in_attack_range}"
+# p "trapped? #{trapped?}"
+# p "ticking captives: #{ticking_captives?}"
+# p "bound enemies: #{@bound_enemies}"
+# p "multiple enemies ahead: #{multiple_enemies_ahead?}"
+# p "direction to captive: #{towards_captive}"
+# p "captive in danger: #{@captive_in_danger}"
+# p "outnumbered: #{outnumbered?}"
+# p "path to captives blocked: #{path_to_captives_blocked?}"
+# p "path clear towards captive: #{path_clear?(towards_captive)}"
+# p "warrior health #{@warrior.health}"
+# p "path traveled last #{@path_traveled.last}"
+# p "serverely wounded: #{severely_wounded?}"
+# p look_for_enemies_towards_captives
+# p "distance to captive #{how_far_to('Captive')}"
+# p too_close_to_captive_for_bombs('Captive')
+# p @warrior.health
+# p "this is the path_traveled #{@path_traveled}"
+# p "this is the queue #{@queue}"
+# p "continue bombing? #{continue_bombing?}"
+
+# p @directions_to_all_enemies
+# p "enemies_in_room #{enemies_in_room?}"
+
+     
