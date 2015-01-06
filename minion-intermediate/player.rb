82d8b756b7d2c7f850da316c04e9b791c3a36961
Richard Bacci <brownieelf175@gmail.com>
2015-01-06 16:44:23 -0500

    thru level 4

--- minion-intermediate/player.rb
+++ minion-intermediate/player.rb
@@ -22,11 +22,13 @@ class Player
                             #:stairs]
     @objectives ||= []
     @stairs ||= false
+    @warrior_health ||= 20
   end
 
 
   def play_turn(warrior)
   	@warrior = warrior
+
 
     def count_objects
       @warrior.listen.size
@@ -65,9 +67,9 @@ class Player
     p "Direction to stairs: #{@warrior.direction_of_stairs}"
     puts
 
-    def nobody_in_sight
-      warrior_listens.empty?
-    end
+    # def nobody_in_sight
+    #   warrior_listens.empty?
+    # end
 
     def towards_stairs
       @warrior.direction_of_stairs
@@ -123,6 +125,10 @@ class Player
 
     build_objectives if @objectives.empty?
 
+    def objectives_accomplished
+      @objectives.empty?
+    end
+
     p "remaining objectives"
     puts
     @objectives.each do |s|
@@ -135,16 +141,172 @@ class Player
     # p @warrior_hears[0].name
     # p @warrior_hears.each {|x| puts x}
 
+    def towards_objective
+      next_objective.direction
+    end
 
-   
-    if @objectives.empty?
+    def danger_close
+      next_objective.enemy_threat && next_objective.distance == 1
+    end
+
+    def walk_to_stairs
       @warrior.walk!(towards_stairs)
+    end
+
+    def next_to_captive
+      next_objective.captive && next_objective.distance == 1
+    end
+
+    def rescue_captive
+      @warrior.rescue!(towards_objective)
+    end
+
+    def danger_far
+      next_objective.enemy_threat && next_objective.distance > 1
+    end
+
+    def path_clear
+      @warrior.feel(towards_objective).empty?
+    end
+
+    def walk_towards_objective
+      @warrior.walk!(towards_objective)
+    end
+
+    def blow_stuff_up
+      @warrior.detonate!(towards_objective)
+    end
+
+    def attack_enemy
+      @warrior.attack!(towards_objective)
+    end
+
+    def warrior_critical
+      @warrior_health <= 5
+    end
+
+    def warrior_wounded
+      @warrior.health < 13
+    end
+
+    def warrior_rest
+      @warrior.rest!
+    end
+
+    def safe_to_rest
+      !warrior_feels.has_value?('Sludge') && 
+      !warrior_feels.has_value?('Thick Sludge')
+    end
+
+    def direction_to_retreat
+      warrior_feels.each { |direction, space| return direction if space == 'nothing' }
+      nil
+    end
+
+    def warrior_retreat
+      @warrior.walk!(direction_to_retreat)
+    end
+
+    def three_front_war
+      total_enemies = 0
+      escape_route = false
+
+      warrior_feels.each do |direction, space|
+        total_enemies += 1 if space.eql?('Thick Sludge') || space.eql?('Sludge')
+        escape_route = true if space.eql?('nothing')
+      end
+      return true if total_enemies == 3 && escape_route == true
+      false
+    end
+
+    def perfect_bomb_location
+      total_enemies = 0
+      @warrior_hears.each do |space|
+        total_enemies += 1 if space.distance == 2
+      end
+      return true if total_enemies == 3
+      false
+    end
+
+    def look_for_direction
+      @warrior_sees.each do |direction, squares|
+        return direction if squares[1][0] == 'Thick Sludge' || squares[1][0] == 'Sludge'
+      end
+    end
+
+    def count_enemies_in_range
+      total_enemies = 0
+
+      warrior_feels.each do |direction, space|
+        total_enemies += 1 if space.eql?('Thick Sludge') || space.eql?('Sludge')
+      end
+      total_enemies
+    end
     
+    #p count_enemies_in_range
 
+    #p next_objective.distance unless next_objective.nil?
+   #p warrior_feels
+   puts
+    if objectives_accomplished
+      walk_to_stairs
+    elsif next_to_captive
+      rescue_captive
+    elsif three_front_war
+      warrior_retreat
+    elsif perfect_bomb_location
+      @warrior.detonate!(look_for_direction)
+    elsif warrior_wounded && safe_to_rest
+      warrior_rest
+    elsif warrior_critical
+      warrior_retreat
+    elsif danger_far
+      if path_clear
+        walk_towards_objective
+      else
+        blow_stuff_up
+      end
+    elsif danger_close
+      if count_enemies_in_range > 1
+        blow_stuff_up
+      else
+        attack_enemy
+      end
     else
-
+      walk_towards_objective
     end
       
+    @warrior_health = warrior.health
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       # if @warrior.feel(towards_stairs).empty?
       #   @warrior.walk!(towards_stairs)
 
